ce22dcd066738452d793366a050a95eca9e26b87
Steven Sun <stevensun914@utexas.edu>
2019-08-05 11:01:03 -0400

    added new heading classes to css and finished missions about page

--- src/pages/mission.js
+++ src/pages/mission.js
@@ -9,11 +9,16 @@ export class Mission extends Component {
             color: 'red',
             fontSize: 35
         },
+        subheader: {
+            color: 'red',
+            fontSize: 25
+        },
         bold: {
             fontWeight: 'bold'
         },
         blue: {
-            color: 'blue'
+            color: '#1cb7e6',
+            fontSize: 50
         }
     }
 
@@ -24,7 +29,35 @@ export class Mission extends Component {
                 <div class="row">
                     <vl/>
                     <div class="col-8">
-                        <h1 class="heading mt-3" style={this.styles.header}>ADVANCED GEOPHYSICAL TECHNOLOGY</h1>
+                        <h1 class="heading mt-5 ml-4" style={this.styles.header}>ADVANCED GEOPHYSICAL TECHNOLOGY</h1>
+                        <div class="ml-4">
+                            <span class="txt subheading-bold">Advanced Geophysical Technology </span>
+                            <span class="txt subheading">(“AGT”) is a growing Houston-based company that is revolutionizing seismic imaging. From our origin as a developer of innovative GPU-based depth imaging algorithms, we are evolving into a service organization that uses our world-class computing power and proprietary algorithms to develop high-resolution 3D seismic images quickly and cost-effectively. Together, our team of elite geophysicists is creating unparalleled value by helping oil and gas companies select and employ the ideal imaging technology to accurately image the geophysical properties of salt basins and other hard-to-image subsurface locations.</span>
+                        </div>
+                        <div class="ml-4">
+                            <span class="txt heading-2" style={this.styles.blue}>+ </span>
+                            <span class="txt heading-3" style={this.styles.subheader}>Our Mission</span>
+                        </div>
+                        <div class="ml-4">
+                            <span class="txt subheading-bold">AGT </span>
+                            <span class="txt subheading">will invest in leading-edge depth imaging technologies and proprietary algorithms that help support oil and gas companies seeking new energy discoveries. Our technologies and services help them derive more value from data, accelerate project timetables, and mitigate financial risks.</span>
+                        </div>
+                        <div class="ml-4">
+                            <span class="txt heading-2" style={this.styles.blue}>+ </span>
+                            <span class="txt heading-3" style={this.styles.subheader}>Our Vision</span>
+                        </div>
+                        <div class="ml-4">
+                            <span class="txt subheading-bold">AGT </span>
+                            <span class="txt subheading">will open the window to a dynamic future of energy exploration, in which highly sophisticated depth imaging technologies help oil and gas companies turn vast energy reserves deep below the surface into abundant resources that enhance quality of life and power economic growth.</span>
+                        </div>
+                        <div class="ml-4">
+                            <span class="txt heading-2" style={this.styles.blue}>+ </span>
+                            <span class="txt heading-3" style={this.styles.subheader}>Our Values</span>
+                        </div>
+                        <div class="ml-4">
+                            <span class="txt subheading-bold">AGT </span>
+                            <span class="txt subheading">is an entrepreneurial company that is using our collective geophysics technology expertise to shape the future of seismic imaging. We apply the knowledge of our carefully selected team to generate fast, cost-effective images of the highest available resolution and quality—including proprietary patent-pending technologies that allow us to obtain high-quality images in record times.  By seeing far below the surface, oil and gas companies can secure the world’s energy future while protecting employees and the environment.</span>
+                        </div>
                     </div>
                     <div class="col-0">
                         <img src={require("../images/mission-timeline2017.png")} alt="timeline"/>
